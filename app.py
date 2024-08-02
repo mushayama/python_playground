efb410eea37aec59514519b882782cbbc61df9e7
Mohd Arbab <mohd.arbab.work@gmail.com>
2024-08-02 17:44:44 +0530

    first iteration madlibs game

---
+++ app.py
@@ -0,0 +1,10 @@
+def madLibs():
+    color = input("Enter a colour: ")
+    noun = input("Enter a plural noun: ")
+    action = input("Enter an action: ")
+
+    print("Roses are "+color)
+    print(noun+" are blue")
+    print("Close your eyes, or i'll "+action+" you")
+
+madLibs()   #check comment
